42772d49e3c1725fbe61667c1c249e5754cdcd22
İbrahim Çekiç <ibrahimcekic@Ibrahimlgisayar.home>
2024-01-02 23:09:00 +0300

    test commit 3

--- App.tsx
+++ App.tsx
@@ -80,7 +80,7 @@ const App = () => {
           }}>
           <Section title="Step One">
             Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
+            Hİ
           </Section>
           <Section title="See Your Changes">
             <ReloadInstructions />
